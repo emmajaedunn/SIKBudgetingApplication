b4305cb94205624b78d431aff0e14a57a3779e54
emmajaedunn <emmajaedunn@gmail.com>
2025-05-28 14:12:17 +0200

    Fixed functionalities (added dashboard, moved budget goals to budget page, fixed buttons on all pages)

--- app/src/main/java/com/example/st10298850_prog7313_p2_lp/viewmodels/TransactionHistoryViewModelFactory.kt
+++ app/src/main/java/com/example/st10298850_prog7313_p2_lp/viewmodels/TransactionHistoryViewModelFactory.kt
@@ -4,6 +4,90 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.st10298850_prog7313_p2_lp.TransactionHistoryViewModel
+import com.example.st10298850_prog7313_p2_lp.repositories.AccountRepository
+import com.example.st10298850_prog7313_p2_lp.repositories.TransactionRepository
+
+class TransactionHistoryViewModelFactory(
+    private val application: Application,
+    private val transactionRepository: TransactionRepository,
+    private val accountRepository: AccountRepository,
+    private val userId: Long
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TransactionHistoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return TransactionHistoryViewModel(
+                application,
+                transactionRepository,
+                accountRepository,
+                userId
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*class TransactionHistoryViewModelFactory(
+    private val application: Application,
+    private val userRepository: AccountRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TransactionHistoryViewModel::class.java)) {
+            return TransactionHistoryViewModel(userRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}*/
+
+
+
+
+
+/*class TransactionHistoryViewModelFactory(
+    private val application: Application,
+    private val userRepository: AccountRepository,
+    // ADDED THIS
+    private val userId: Long
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return HomeViewModel(application, userRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*package com.example.st10298850_prog7313_p2_lp.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.st10298850_prog7313_p2_lp.TransactionHistoryViewModel
 import com.example.st10298850_prog7313_p2_lp.data.AppDatabase
 import com.example.st10298850_prog7313_p2_lp.repositories.TransactionRepository
 
@@ -20,4 +104,4 @@ class TransactionHistoryViewModelFactory(
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
-}
+}*/
